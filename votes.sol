578f77fb7bde4eb7c1e53336427084f26c003890
MakivDev <8064637@stud.kai.edu.ua>
2026-01-13 20:30:31 +0200

    Implemented voting logic: added owner restrictions, max votes limit, and election time functionality using block.timestamp. Added manual stopVote function for contract owner.

--- votes.sol
+++ votes.sol
@@ -1,44 +1,55 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-contract voting{
-
+contract voting {
     string[] public electors;
 
     uint256 public maxVotes;
-
     uint256 public electionTime;
 
-    
+    bool public validVote;
 
+    address public owner;
 
-    mapping (address=>uint256) public userVotes;
-    mapping (uint256=>uint256) public numberOfVotes;
-    mapping (address=>bool) public accessToVote;
-    
-    constructor(string[] memory _electors, uint256 _maxVotes, uint256 _electionTime){
+    mapping(address => uint256) public userVotes;
+    mapping(uint256 => uint256) public numberOfVotes;
+    mapping(address => bool) public accessToVote;
+
+    constructor(
+        string[] memory _electors,
+        uint256 _maxVotes,
+        uint256 _electionTime
+    ) {
         electors = _electors;
         maxVotes = _maxVotes;
-        electionTime = _electionTime;
+        electionTime = block.timestamp + _electionTime;
+        validVote = true;
+        owner = msg.sender;
     }
 
-    function vote (uint256 _number) public {
-        require(_number<electors.length, "number of electors invalid");
-        require(accessToVote[msg.sender]==false, "User already vote");
-        userVotes[msg.sender]=_number;
-        numberOfVotes[_number]+=1;
+    function vote(uint256 _number) public {
+        require(_number < electors.length, "number of electors invalid");
+        require(accessToVote[msg.sender] == false, "User already vote");
+        require(
+            validVote == true,
+            "everyone already have max vote or vote stopped by owner"
+        );
+        require(msg.sender != owner, "owner cant vote");
+        require(electionTime > block.timestamp, "time out");
+
+        userVotes[msg.sender] = _number;
+        numberOfVotes[_number] += 1;
+
+        accessToVote[msg.sender] = true;
+
+        if (numberOfVotes[_number] >= maxVotes) {
+            validVote = false;
+        }
     }
 
+    function stopVote() public {
+        require(msg.sender == owner, "you not owner");
 
-
-
-
-
-
-
-
-
-
-
-
+        validVote = false;
+    }
 }
