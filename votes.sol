7c4544de773d9788d39e5ada20acdaea9ccd340a
MakivDev <8064637@stud.kai.edu.ua>
2026-01-28 17:48:14 +0200

    Refactored Election contract: implemented real-time leader tracking to replace gas-intensive loops, added activeVoting state management with custom modifiers, and implemented flexible time adjustment using signed integers (int256). Added events for frontend integration.

--- votes.sol
+++ votes.sol
@@ -8,10 +8,16 @@ contract Election {
     uint256 public maxVotes;
     uint256 public totalVotes;
     uint256 public electionEndTime;
-    uint256 public startTime;
+    uint256 public currentLeaderID;
+    uint256 public leadingVotes;
+
+    bool public activeVoting;
 
     mapping(address => bool) public userVotes;
     mapping(uint256 => uint256) public numberOfVotes;
+
+    event Voted(uint256 _index, address _voter);
+    event Message(string _message);
 
     error OnlyOwnerAllowed();
     error ElectorDoesNotExist(uint256 _pickedElector, uint256 _totalElectors);
@@ -22,68 +28,87 @@ contract Election {
     error VotingIsNotOver();
     error MaxVotesCantDecrease();
     error MustBeLater();
+    error NoVotesFound();
 
     constructor(
         string[] memory _electors,
         uint256 _maxVotes,
         uint256 _durationInSeconds
-        
     ) {
-        startTime = block.timestamp;
         electors = _electors;
         maxVotes = _maxVotes;
         electionEndTime = block.timestamp + _durationInSeconds;
         owner = msg.sender;
+        activeVoting = true;
     }
 
-    function vote(uint256 _number) public {
-        require(userVotes[msg.sender] == false, CantVoteTwice());
-        require(_number < electors.length, ElectorDoesNotExist(_number, electors.length));
-        require(totalVotes < maxVotes, MaxVotesReached(maxVotes));
-        require(block.timestamp < electionEndTime, VotingIsOver());
-        require(msg.sender != owner, OwnerCantVote());
-
-        userVotes[msg.sender] = true;
-        numberOfVotes[_number] += 1;
-        totalVotes += 1;
-    }
-
-    modifier  onlyOwner(){
+    modifier onlyOwner() {
         require(msg.sender == owner, OnlyOwnerAllowed());
         _;
     }
 
-
-    function stopVote() public onlyOwner {
-        electionEndTime = block.timestamp;
+    modifier onlyWhileActive() {
+        require(activeVoting == true, VotingIsOver());
+        _;
     }
 
-    function resetMaxVotes(uint256 _newMaxVotes) public onlyOwner  {
+    function vote(uint256 _number) public onlyWhileActive {
+        require(!userVotes[msg.sender], CantVoteTwice());
+        require(
+            _number < electors.length,
+            ElectorDoesNotExist(_number, electors.length)
+        );
+        require(block.timestamp < electionEndTime, VotingIsOver());
+        require(msg.sender != owner, OwnerCantVote());
+        require(totalVotes < maxVotes, MaxVotesReached(maxVotes));
+
+        userVotes[msg.sender] = true;
+        numberOfVotes[_number] += 1;
+        totalVotes += 1;
+
+        if (numberOfVotes[_number] > leadingVotes) {
+            leadingVotes = numberOfVotes[_number];
+            currentLeaderID = _number;
+        }
+        if (totalVotes == maxVotes) {
+            activeVoting = false;
+        }
+        emit Voted(_number, msg.sender);
+    }
+
+    function stopVote() public onlyOwner onlyWhileActive {
+        electionEndTime = block.timestamp;
+        emit Message("Vote is stopped");
+        activeVoting = false;
+    }
+
+    function resetMaxVotes(uint256 _newMaxVotes) public onlyOwner {
         require(_newMaxVotes > maxVotes, MaxVotesCantDecrease());
         maxVotes = _newMaxVotes;
+        activeVoting = true;
+        emit Message("Max votes is resetting");
     }
 
-    function resetEndTime (uint256 _newEndTime) public onlyOwner  {
-        require(_newEndTime>block.timestamp, MustBeLater());
-        electionEndTime = startTime + _newEndTime;
-    }
-
-
-    function getLeader () public view returns(string memory){
-        require(block.timestamp>electionEndTime, VotingIsNotOver());
-        
-        uint256 winnerID;
-
-        for(uint256 i=0; i<electors.length; i++){
-            if(numberOfVotes[i]>winnerID){
-                winnerID=i;
-            }
+    function editEndTime(int256 _newEndTime) public onlyOwner {
+        if (_newEndTime > 0) {
+            electionEndTime += uint256(_newEndTime);
+            activeVoting = true;
+        } else {
+            uint256 reduction = uint256(-_newEndTime);
+            if (electionEndTime - reduction < block.timestamp)
+                revert MustBeLater();
+            electionEndTime -= reduction;
         }
-        return electors[winnerID];
+        emit Message("EndTime edited");
     }
 
+    function getLeader() public view returns (string memory) {
+        require(
+            block.timestamp > electionEndTime || !activeVoting,
+            VotingIsNotOver()
+        );
+        require(leadingVotes != 0, NoVotesFound());
 
-
-
-
+        return electors[currentLeaderID];
+    }
 }
