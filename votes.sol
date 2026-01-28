9675c3c8039a91c2e8c0366659781415073c1cfc
MakivDev <8064637@stud.kai.edu.ua>
2026-01-28 14:55:35 +0200

    add startTime, add function getLeader

--- votes.sol
+++ votes.sol
@@ -1,58 +1,89 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-contract voting {
+contract Election {
     string[] public electors;
-
-    uint256 public maxVotes;
-    uint256 public electionTime;
-
-    bool public validVote;
-
     address public owner;
 
-    mapping(address => uint256) public userVotes;
-    mapping(uint256 => uint256) public numberOfVotes;
-    mapping(address => bool) public accessToVote;
+    uint256 public maxVotes;
+    uint256 public totalVotes;
+    uint256 public electionEndTime;
+    uint256 public startTime;
 
-    error ElecrotDoesNotExist(uint256 _pickedElector, uint256 _totalElectors);
-    error UserAlreadyVote();
-    error OnlyOwnerCan();
-    error OwnerCantIt();
-    error TimeOut();
+    mapping(address => bool) public userVotes;
+    mapping(uint256 => uint256) public numberOfVotes;
+
+    error OnlyOwnerAllowed();
+    error ElectorDoesNotExist(uint256 _pickedElector, uint256 _totalElectors);
+    error OwnerCantVote();
+    error CantVoteTwice();
+    error MaxVotesReached(uint256 _maxVotes);
+    error VotingIsOver();
+    error VotingIsNotOver();
+    error MaxVotesCantDecrease();
+    error MustBeLater();
 
     constructor(
         string[] memory _electors,
         uint256 _maxVotes,
-        uint256 _electionTime
+        uint256 _durationInSeconds
+        
     ) {
+        startTime = block.timestamp;
         electors = _electors;
         maxVotes = _maxVotes;
-        electionTime = block.timestamp + _electionTime;
-        validVote = true;
+        electionEndTime = block.timestamp + _durationInSeconds;
         owner = msg.sender;
     }
 
     function vote(uint256 _number) public {
-        require(_number < electors.length, ElecrotDoesNotExist(_number,electors.length));
-        require(accessToVote[msg.sender] == false, UserAlreadyVote());
-        require(validVote == true,"everyone already have max vote or vote stopped by owner");
-        require(msg.sender != owner, OwnerCantIt());
-        require(electionTime > block.timestamp, TimeOut());
+        require(userVotes[msg.sender] == false, CantVoteTwice());
+        require(_number < electors.length, ElectorDoesNotExist(_number, electors.length));
+        require(totalVotes < maxVotes, MaxVotesReached(maxVotes));
+        require(block.timestamp < electionEndTime, VotingIsOver());
+        require(msg.sender != owner, OwnerCantVote());
 
-        userVotes[msg.sender] = _number;
+        userVotes[msg.sender] = true;
         numberOfVotes[_number] += 1;
+        totalVotes += 1;
+    }
 
-        accessToVote[msg.sender] = true;
+    modifier  onlyOwner(){
+        require(msg.sender == owner, OnlyOwnerAllowed());
+        _;
+    }
 
-        if (numberOfVotes[_number] >= maxVotes) {
-            validVote = false;
+
+    function stopVote() public onlyOwner {
+        electionEndTime = block.timestamp;
+    }
+
+    function resetMaxVotes(uint256 _newMaxVotes) public onlyOwner  {
+        require(_newMaxVotes > maxVotes, MaxVotesCantDecrease());
+        maxVotes = _newMaxVotes;
+    }
+
+    function resetEndTime (uint256 _newEndTime) public onlyOwner  {
+        require(_newEndTime>block.timestamp, MustBeLater());
+        electionEndTime = startTime + _newEndTime;
+    }
+
+
+    function getLeader () public view returns(string memory){
+        require(block.timestamp>electionEndTime, VotingIsNotOver());
+        
+        uint256 winnerID;
+
+        for(uint256 i=0; i<electors.length; i++){
+            if(numberOfVotes[i]>winnerID){
+                winnerID=i;
+            }
         }
+        return electors[winnerID];
     }
 
-    function stopVote() public {
-        require(msg.sender == owner, OnlyOwnerCan());
 
-        validVote = false;
-    }
+
+
+
 }
